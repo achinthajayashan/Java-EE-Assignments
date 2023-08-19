fa52f1a252f861f7fc87c478ef12bb31e91dd6cc
achinthajayashan <achintha.uns.2018@gmail.com>
2023-08-19 14:38:32 +0530

    customer get all

--- Json-with-Ajax/src/servlet/CustomerServlet.java
+++ Json-with-Ajax/src/servlet/CustomerServlet.java
@@ -1,18 +1,55 @@
 package servlet;
 
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
 
-@WebServlet(urlPatterns = "")
+@WebServlet(urlPatterns = "/pages/customer")
 public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Jsonajax", "root", "12345678");
+            PreparedStatement pstm = connection.prepareStatement("select * from customer");
+            ResultSet rst = pstm.executeQuery();
+
+            resp.addHeader("Content-Type","application/json");
+
+            JsonArrayBuilder allCustomers = Json.createArrayBuilder();
+
+
+            while (rst.next()) {
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+                int salary = rst.getInt(4);
+
+                JsonObjectBuilder customer = Json.createObjectBuilder();
+
+                customer.add("id",id);
+                customer.add("name",name);
+                customer.add("address",address);
+                customer.add("salary",salary);
+
+                allCustomers.add(customer.build());
+            }
+
+            resp.getWriter().println(allCustomers.build());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
