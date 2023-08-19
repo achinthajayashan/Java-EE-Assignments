3bed059cfd5fd2ded8c7d4cd2639c718826ab8fd
achinthajayashan <achintha.uns.2018@gmail.com>
2023-08-19 16:01:35 +0530

    item GetAll

---
+++ Json-with-Ajax/src/servlet/ItemServlet.java
@@ -0,0 +1,56 @@
+package servlet;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.*;
+
+@WebServlet(urlPatterns = "/pages/item")
+public class ItemServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Jsonajax", "root", "12345678");
+            PreparedStatement pstm = connection.prepareStatement("select * from item");
+            ResultSet rst = pstm.executeQuery();
+
+            resp.addHeader("Content-Type","application/json");
+
+            JsonArrayBuilder allItems = Json.createArrayBuilder();
+
+
+            while (rst.next()) {
+                String code = rst.getString(1);
+                String description = rst.getString(2);
+                String qty = rst.getString(3);
+                int unitPrice = rst.getInt(4);
+
+                JsonObjectBuilder item = Json.createObjectBuilder();
+
+                item.add("code",code);
+                item.add("description",description);
+                item.add("qty",qty);
+                item.add("unitPrice",unitPrice);
+
+                allItems.add(item.build());
+            }
+
+            resp.getWriter().println(allItems.build());
+
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
